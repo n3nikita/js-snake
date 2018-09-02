5369a1f34a471768ee2a86d09e3d53b9034bc8b8
Nikita <nbandaruk@gmail.com>
2018-09-02 14:18:07 +0300

    Add game over check function

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 $(document).ready(function () { 
-    var size = 70;
-    var arr = [];
-    var direction = 'r';
-    var snakeSize = 5;
+
+    var arr, 
+        direction, 
+        snakeSize, 
+        gameSpeed, 
+        pause, 
+        interval,
+        size = 50;
     var field = [];
-    var gameSpeed = 10;
     var food = {};
-    var pause = false;
 
     $('#snakeGround').css({
         'width': size * 10,
@@ -14,35 +16,42 @@ $(document).ready(function () {
     });
 
     $('html').keydown(function(e){
-        if(e.which === 40)
+        if(e.which === 40 && direction !== 'd')
             direction = 'u';
-        else if(e.which === 38)
+        else if(e.which === 38 && direction !== 'u')
             direction = 'd';
-        else if(e.which === 37)
+        else if(e.which === 37 && direction !== 'r')
             direction = 'l';
-        else if(e.which === 39)
+        else if(e.which === 39 && direction !== 'l')
             direction = 'r';
         else if(e.which === 32)
             pause = !pause;
     });
 
-    
-    (function Init() { 
+    function start() { 
+        pause = false;
+        arr = [];
+        snakeSize = 10;
+        gameSpeed = 10;
+        direction = 'r';
+        field = [];
         for(var i = snakeSize - 1; i >= 0; i--){
             arr.push({x: i, y: 0});
         }
         fill();
 
-        
-        setInterval(() => {
+        clearInterval(interval);
+        interval = setInterval(() => {
             if(!pause)
                 moveSnake();
         }, 1000 / gameSpeed);
-    })();
+    }
+
+    (start)();
+
 
     function fill(){
         $('#snakeGround').html('');
-
         for(var i = 0; i < size; i++){
             field[i] = [];
             row = $("<div>", { 'class': 'row' }).appendTo("#snakeGround");
@@ -92,27 +101,43 @@ $(document).ready(function () {
         }
 
         // go thought the walls
-        if(sx >= 70)
+        if(sx >= field.length)
             sx = 0;
 
         if(sx < 0)
-            sx = 69;
+            sx = field.length - 1;
         
-        if(sy >= 70)
+        if(sy >= field.length)
             sy = 0;
         
         if(sy < 0)
-            sy = 69;
+            sy = field.length - 1;
         
 
         tail.x = sx;
         tail.y = sy;
-        arr.unshift(tail);
-        drawSnake();
+        
+        if(checkGameOver(tail)){
+            arr = [];
+            start();
+        } else {
+            arr.unshift(tail);
+            drawSnake();
+        }
     }
 
     function placeFood(){
         food = {x: Math.floor(Math.random() * size), y: Math.floor(Math.random() * size)};
         field[food.y][food.x].addClass('food');
+    }
+
+    function checkGameOver(head){
+        for(var i = 0; i < arr.length; i++){
+            if(arr[i].x === head.x && arr[i].y === head.y){
+                return true;
+            }
+        }
+
+        return false;
     }
 });
